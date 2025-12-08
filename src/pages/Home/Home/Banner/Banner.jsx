9d37d0c8256f543f797b52f2de5dcb5f1f2250dc
mostafijurbabu <mostafijurbabu45@gmail.com>
2025-12-08 22:53:35 +0600

    added banner use carousel

--- src/pages/Home/Home/Banner/Banner.jsx
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import bannerImg1 from "../../../../assets/loanimg4.jpg";
+import bannerImg2 from "../../../../assets/loanimg9.jpg";
+import bannerImg3 from "../../../../assets/loanimg10.jpg";
 
 const Banner = () => {
   return (
-    <div>
-      <h2>This is Banner</h2>
-    </div>
+    <Carousel
+      autoPlay={true}
+      infiniteLoop={true}
+      interval={3000}
+      dynamicHeight={70}
+    >
+      <div>
+        <img className="rounded-xl" src={bannerImg1} />
+        <p className="legend">Legend 1</p>
+      </div>
+      <div>
+        <img className="rounded-xl" src={bannerImg2} />
+        <p className="legend">Legend 2</p>
+      </div>
+      <div>
+        <img className="rounded-xl" src={bannerImg3} />
+        <p className="legend">Legend 3</p>
+      </div>
+    </Carousel>
   );
 };
 
